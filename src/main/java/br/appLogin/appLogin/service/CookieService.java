ed2fa612b6fe814d36450840098d3d5bdf9fffb8
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2026-05-14 22:05:00 -0300

    update project cokiee

--- src/main/java/br/appLogin/appLogin/service/CookieService.java
+++ src/main/java/br/appLogin/appLogin/service/CookieService.java
@@ -1,4 +1,9 @@
 package br.appLogin.appLogin.service;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 public class CookieService {
+    public static void setCookie(HttpServletResponse response, String key, String valor, int segundos){
+
+    };
 }

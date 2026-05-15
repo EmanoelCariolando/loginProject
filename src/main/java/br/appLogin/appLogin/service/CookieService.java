5edf5d03346338e76b1785be62ca94f7bc0e72ff
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2026-05-15 20:53:31 -0300

    Encode and set HTTP cookie with max-age
    Create and add a Cookie to the HttpServletResponse: encode the cookie value with URLEncoder (UTF-8), set the cookie max-age, and add it to the response. The setCookie method now declares throws UnsupportedEncodingException and required imports were added.

--- src/main/java/br/appLogin/appLogin/service/CookieService.java
+++ src/main/java/br/appLogin/appLogin/service/CookieService.java
@@ -1,9 +1,19 @@
 package br.appLogin.appLogin.service;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class CookieService {
-    public static void setCookie(HttpServletResponse response, String key, String valor, int segundos){
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.Optional;
 
+public class CookieService {
+    public static void setCookie(HttpServletResponse response, String key, String valor, int segundos) throws UnsupportedEncodingException {
+        Cookie cookie = new Cookie(key, URLEncoder.encode(valor,"UTF-8"));
+        cookie.setMaxAge(segundos);
+        response.addCookie(cookie);
     };
 }
